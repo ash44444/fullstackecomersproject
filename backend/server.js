fa07b95fed9932f035ae1edaf8bb5e392d2e21df
ash44444 <ashishmaner21@gmail.com>
2023-03-16 19:41:06 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,44 @@
-const express = require("express");
-const { errorHandler } = require("./middlewares/errorMiddleware");
-require("colors");
-const products = require("./data/products");
-const dotenv = require("dotenv");
-const connectDb = require("./config/config");
-const productRoutes = require("./routes/productsRoute");
-const usersRoutes = require("./routes/UsersRoute");
-const orderRoutes = require("./routes/orderRoute");
+const express = require('express')
+const { errorHandler } = require('./middlewares/errorMiddleware')
+require('colors')
+const products = require('./data/products')
+const dotenv = require('dotenv')
+const connectDb = require('./config/config')
+const productRoutes = require('./routes/productsRoute')
+const usersRoutes = require('./routes/UsersRoute')
+const orderRoutes = require('./routes/orderRoute')
+const path = require('path')
 
-dotenv.config();
+dotenv.config()
 //connecting to mongodb database
-connectDb();
-const app = express();
+connectDb()
+const app = express()
 //middleware bodyparser
-app.use(express.json());
+app.use(express.json())
 
 //dotenv config
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Node Server</h1>");
-});
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to Node Server</h1>')
+})
 
-app.use("/api", productRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/orders", orderRoutes);
-app.get("/api/config/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID);
-});
+app.use('/api', productRoutes)
+app.use('/api/users', usersRoutes)
+app.use('/api/orders', orderRoutes)
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID)
+})
 
-app.use(errorHandler);
+app.use(errorHandler)
+app.use(express.static(path.join(__dirname, './frontend/build')))
 
-const PORT = 8080;
+app.get('*', function (req, res) {
+  res.sendFile(__dirname, './frontend/build/index.html')
+})
+
+const PORT = 8080
 app.listen(process.env.PORT || PORT, () => {
   console.log(
     `Server Running in ${process.env.NODE_ENV} Mode on Port ${process.env.PORT}`
-      .inverse
-  );
-});
+      .inverse,
+  )
+})
